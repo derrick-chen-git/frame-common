7e708a487a7b9b98ded00feaa385aa3dad1b759e
derrick <derrick_chenkj@163.com>
2018-11-22 20:24:27 +0800

    去除redisson配置

--- src/main/java/com/frame/common/redis/RedissonConfig.java
+++ src/main/java/com/frame/common/redis/RedissonConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.frame.common.redis;
 
 import lombok.Data;
@@ -27,3 +28,4 @@ public class RedissonConfig {
         return redisson;
     }
 }
+*/
--- src/main/java/com/frame/common/shardingDatasource/ShardingMasterSlaveConfig.java
+++ src/main/java/com/frame/common/shardingDatasource/ShardingMasterSlaveConfig.java
@@ -8,11 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created with Intellij IDEA.
- * @author hjc
- * @version 2018/5/8 0008
- */
+
 @Data
 @ConfigurationProperties(prefix = "sharding.jdbc")
 public class ShardingMasterSlaveConfig {
